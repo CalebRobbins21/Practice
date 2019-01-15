3c1cbe97b34ee84a22bbd8eb33711398bd33c22a
CalebRobbins21 <45544750+CalebRobbins21@users.noreply.github.com>
2019-01-15 17:50:29 -0500

    Update es6script.js

--- es6script.js
+++ es6script.js
@@ -337,7 +337,7 @@ function RobbinsPerson(firstName, yearOfBirth, lastName, nationality) {
 
 var caleb = new RobbinsPerson('Caleb', 1996);
 var Jennie = new RobbinsPerson ('Jennie', 1994, 'Mantell', 'Italian');
-*/
+
 //ES6
 function RobbinsPerson(firstName, yearOfBirth, lastName = 'Robbins', nationality = 'American') {
     this.firstName = firstName;
@@ -348,3 +348,146 @@ function RobbinsPerson(firstName, yearOfBirth, lastName = 'Robbins', nationality
 
 var caleb = new RobbinsPerson('Caleb', 1996, 'Robbins', 'American');
 var jennie = new RobbinsPerson('Jennie', 1994);
+*/
+/////////////////////////////////////////////////////////////////////
+// Maps
+/*
+const question = new Map();
+
+question.set('question', 'What is the official name of the latest major JavaScript version?');
+question.set(1, 'ES5');
+question.set(2, 'ES6');
+question.set(3, 'ES2015');
+question.set(4, 'ES7');
+question.set('correct', 3);
+question.set(true, 'Correct Answer');
+question.set(false, 'Wrong answer, please try again!');
+
+console.log(question.get('question'));
+console.log(question.size);
+
+if(question.has(4)){
+    //question.delete(4);
+    //console.log('Answer 4 is here');
+}
+
+//question.clear();
+
+//question.forEach((value, key) => console.log(`This is ${key}, and it is set to ${value}`));
+
+for (let [key, value] of question.entries()) {
+    if (typeof(key) === 'number') {
+        console.log(`Answer ${key}: ${value}`);
+    }
+}
+
+const ans = parseInt(prompt('Write the correct answer'));
+console.log(question.get (ans === question.get('correct')));
+*/
+
+////////////////////////////////////////////////////////////////////////
+//Classes
+/*
+//ES5
+var person5 = function(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+person5.prototype.calculateAge = function() {
+    var age = new Date().getFullYear - this.yearOfBirth;
+    console.log(age);
+}
+
+var john5 = new Person5('Caleb', 1996, 'student');
+
+//ES6
+
+class Person6 {
+    constructor (name, yearOfBirth, job) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.job = job;
+    }
+
+    calculateAge(){
+        var age = new Date().getFullYear - this.yearOfBirth;
+        console.log(age);
+    }
+
+    static greeting() {
+        console.log('Hey there!');
+    }
+}
+
+const caleb6 = new Person6('Caleb', 1996, 'student');
+
+Person6.greeting();
+*/
+
+//////////////////////////////////////////////////////////////////////
+// Sub-classes
+//ES5
+/*
+var Person5 = function(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+Person5.prototype.calculateAge = function() {
+    var age = new Date().getFullYear() - this.yearOfBirth;
+    console.log(age);
+}
+
+var Athlete5 = function(name, yearOfBirth, job, olympicGames, medals) {
+    Person5.call(this, name, yearOfBirth, job);
+    this.olympicGames = olympicGames;
+    this.medals = medals; 
+}
+
+Athlete5.prototype = Object.create(Person5.prototype);
+
+Athlete5.prototype.wonMedal = function() {
+    this.medals++;
+    console.log(this.medals);
+}
+var calebAthlete5 = new Athlete5('Caleb', 1996, 'swimmer', 3, 10);
+
+calebAthlete5.calculateAge();
+calebAthlete5.wonMedal();
+
+//ES6
+
+class Person6 {
+    constructor (name, yearOfBirth, job) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.job = job;
+    }
+
+    calculateAge(){
+        var age = new Date().getFullYear() - this.yearOfBirth;
+        console.log(age);
+    }
+}
+
+class Athlete6 extends Person6 {
+    constructor(name, yearOfBirth, job, olympicGames, medals) {
+        super(name, yearOfBirth, job);
+        this.olympicGames = olympicGames;
+        this.medals = medals;
+    }
+
+    wonMedal() {
+        this.medals++;
+        console.log(this.medals);
+    }
+}
+
+const calebAthlete6 = new Athlete6('Caleb', 1996, 'rugby', 3, 10);
+
+calebAthlete6.wonMedal();
+calebAthlete6.calculateAge();
+*/
